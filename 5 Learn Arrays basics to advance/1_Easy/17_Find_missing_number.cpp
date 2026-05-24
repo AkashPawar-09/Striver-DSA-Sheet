a733832a144abdd6c1fb36a9f3f64b9d6b2c0482
Akash Pawar <akashpawar.official.acc@gmail.com>
2026-05-24 09:27:54 +0530

    Updated Learn Arrays basics to advance Folder

--- 5 Learn Arrays basics to advance/1_Easy/17_Find_missing_number.cpp
+++ 5 Learn Arrays basics to advance/1_Easy/17_Find_missing_number.cpp
@@ -10,24 +10,37 @@ Explanation: nums contains 0, 1, 2, 4, 5, 6 thus leaving 3 as the only missing n
 
 */
 
+#include<bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n = nums.size();                    // n = size of nums
+        int expectedSum = n * (n + 1) / 2;      // expected sum formula
+        int actualSum = 0;                      // variable that measures actualSum
+        for (int i = 0; i < n; i++) {           // iteration over nums array
+            actualSum += nums[i];               // actualSum stores all Element sum in it 
+        }
+        return expectedSum - actualSum;         // return the missing number by this method 
+    }
+};
 
 
 
+int main () {
+    int n;
+    cin >> n;
+    vector<int> nums(n) ;
+    for (int  i = 0  ; i < n ; i++ ){
+        cin >> nums[i] ;
+    }
+    Solution obj;   
+    int result = obj.missingNumber(nums);  
+    cout << result << endl;
+    return 0;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Time Compleity : O(N)
+// Spcae Complexity : O(1)
