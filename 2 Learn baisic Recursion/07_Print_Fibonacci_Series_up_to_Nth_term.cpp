a06d7479b5090dc241acc5f8ab472c9d75f7b7d4
Akash Pawar <akashpawar.official.acc@gmail.com>
2026-08-13 16:07:06 +0530

    Updated ALL Folders

--- 2 Learn baisic Recursion/07_Print_Fibonacci_Series_up_to_Nth_term.cpp
+++ 2 Learn baisic Recursion/07_Print_Fibonacci_Series_up_to_Nth_term.cpp
@@ -22,8 +22,10 @@ using namespace std ;
 class Solution {
 public:
     int fib(int n) {
-        if (n==0) return 1 ;
-        return n * fib (n-1) ;        
+        if (n==0) {return 0;}                   // base case of fib(n-2)
+        if (n==1) {return 1;}                   // base case of fib(n-1)
+        int result = fib(n-1) + fib(n-2) ;      // actual recursion
+        return result ;                         // return total updated result after all recursion
     }
 };
 
