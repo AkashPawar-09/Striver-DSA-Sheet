5a8569c0a4171b753b915621f70b15d7a1188bb9
Akash Pawar <akashpawar.official.acc@gmail.com>
2026-08-20 08:55:16 +0530

    Updated Stack & Queues Folder

--- 11 Stack and Queues/1_Introduction_to_Stack/1_Implement_Stack_using_Array.cpp
+++ 11 Stack and Queues/1_Introduction_to_Stack/1_Implement_Stack_using_Array.cpp
@@ -1,62 +1,47 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-class StackArray {
-    int* arr;
-    int capacity;
-    int top;
+class Stack {
+    int arr[1000] ;
+    int top ;
 
 public:
-    StackArray(int cap) {
-        capacity = cap;
-        arr = new int[capacity];
-        top = -1; // empty stack
+    Stack() {
+        top = -1 ;
     }
 
     void push(int x) {
-        if (top == capacity - 1) {
-            cout << "Stack Overflow\n";
-            return;
-        }
+        if(top==999) return ;
         arr[++top] = x;
     }
 
     int pop() {
-        if (isEmpty()) {
-            cout << "Stack Underflow\n";
-            return -1;
-        }
+        if (top == -1) return -1;
         return arr[top--];
     }
 
     int peek() {
-        if (isEmpty()) {
-            cout << "Stack is empty\n";
-            return -1;
-        }
+        if (top == -1) return -1;
         return arr[top];
     }
 
     bool isEmpty() {
         return top == -1;
     }
-
-    int size() {
-        return top + 1;
-    }
-
-    ~StackArray() {
-        delete[] arr;
-    }
 };
 
 int main() {
-    StackArray s(100);
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    cout << s.pop() << endl;   // 30
-    cout << s.peek() << endl;  // 20
-    cout << s.size() << endl;  // 2
+    Stack st;
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout << st.peek() << endl;
+    cout << st.pop() << endl;
+    cout << st.peek() << endl;
+
+    cout << (st.isEmpty() ? "Empty" : "Not Empty") << endl;
+
     return 0;
 }
